fb1fa9fda3d280d629065aa392222831df66e718
dsweetser <daniel.sweetser@gmail.com>
2017-02-17 11:53:14 -0500

    implement sessions show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :set_session, only: [:show, :update, :destroy]
   def index
     @sessions = Session.all
 
     render json: @sessions
   end
+
+  def show
+    render json: @session
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_session
+    @session = Session.find(params[:id])
+  end
+  private :set_session
+
+  # Only allow a trusted parameter "white list" through.
+  def session_params
+    params.require(:session).permit(:player, :game, :rating, :players, :notes)
+  end
+  private :session_params
 end
