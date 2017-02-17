9c1d7ead73641854cecd7fd8e91c95dcaefecd14
dsweetser <daniel.sweetser@gmail.com>
2017-02-17 12:28:24 -0500

    implement sideloading for session to user

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal:true
 
 class SessionSerializer < ActiveModel::Serializer
-  attributes :id, :players, :rating
+  attributes :id, :players, :rating, :user
   has_one :game
-  has_one :user
+  def user
+    object.user.id
+  end
 end
